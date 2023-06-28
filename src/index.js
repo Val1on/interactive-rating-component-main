c02cf253b38957c80bcc8af2a72369a41052bbbf
Cyrus Ballon <118657085+Val1on@users.noreply.github.com>
2023-06-29 02:17:38 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const container = document.querySelector("[data-container]");
 const form = document.querySelector("[data-form]");
 const template = document.querySelector("[data-template]");
+
+function selectRating(event) {
+  event.preventDefault();
+
+  const rating = targetRating(event.target).get("rating");
+}
+
+function submitPop(rating) {
+  const clone = document.importNode(template.textContent, true);
+  wrapper.addEventListener("click", myFunction);
+  container.appendChild(clone);
+
+  function myFunction() {}
+}
+
+form.addEventListener("submit", selectRating);
+form.addEventListener("submit", submitPop);
